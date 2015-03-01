716fba715660dc6f356901268c809db380a94769
Andrew Clark <aclarkd@gmail.com>
2015-03-01 12:09:50 -0800

    rename base, update config for editor

---
+++ app/modules/autonomy/autonomy.js
@@ -0,0 +1,11 @@
+(function (angular) {
+
+    'use strict';
+    angular.module('autonomy', [])
+        .controller('autonomyController', function ($scope) {
+
+            $scope.foundation = 'The root is on fire';
+
+        });
+
+})(window.angular);
--- app/modules/video/video.js
+++
@@ -1,6 +0,0 @@
-(function(angular) {
-  'use strict';
-  angular.module('taVideo', []).controller('taVideoController', function($scope) {
-    $scope.foundation = 'The root is on fire';
-  });
-})(window.angular);
